4b054d5caa29190f25e178653f1737eb3cf4d95e
Witek-Milewski <milewskiwitek8@gmail.com>
2021-02-10 22:20:41 +0100

    wszystkie pliki

---
+++ script.js
@@ -0,0 +1,90 @@
+
+window.sr = ScrollReveal();
+
+sr.reveal('#cta_1',{
+origin: 'bottom',
+distance: '50px',
+reset : false ,
+delay: 3500,
+
+});
+
+sr.reveal('.demo-content',{
+    origin: 'bottom',
+    distance: '50px',
+    reset : false ,
+    delay: 600,
+    duration: 1000,
+    });
+
+
+sr.reveal('.features-item',{
+origin: 'left',
+distance: '200px',
+reset : false ,
+delay: 300,
+
+interval: 1000,
+duration: 1000,
+});
+
+
+sr.reveal('#h1_1',{
+    origin: 'top',
+    distance: '50px',
+    reset : false ,
+    delay: 300,
+    
+    interval: 1000,
+    duration: 500,
+    });
+
+sr.reveal('#h1_2',{
+    origin: 'top',
+    distance: '50px',
+    reset : false ,
+    delay: 300,
+       
+    interval: 1000,
+    duration: 500,
+    });
+
+    sr.reveal('#h1_3',{
+        origin: 'top',
+        distance: '50px',
+        reset : false ,
+        delay: 300,
+           
+        interval: 1000,
+        duration: 500,
+        });
+
+    sr.reveal('#card_1',{
+        origin: 'bottom',
+        distance: '100px',
+        reset : false ,
+        delay: 100,
+                   
+                interval: 1000,
+                duration: 800,
+        });
+
+        sr.reveal('#card_2',{
+            origin: 'bottom',
+            distance: '100px',
+            reset : false ,
+            delay: 100,
+                   
+                interval: 1000,
+                duration: 800,
+            });
+
+            sr.reveal('#card_3',{
+                origin: 'bottom',
+                distance: '100px',
+                reset : false ,
+                delay: 100,
+                   
+                interval: 1000,
+                duration: 800,
+                });
